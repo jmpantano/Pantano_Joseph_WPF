6eaba9eb4fc4f9035a40c9fcd080beeb8c1f87a9
Joseph Pantano <joseph.pantano@live.com>
2014-08-13 14:08:31 -0700

    Slice of Pie Part 1 added

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,10 +4,14 @@
 var years = 1; //human years
 var dogYears = years * 7; //dog years
 var sparkyAge = "Sparky is " + years + " human years old, which is " + dogYears + " in dog years."; //Sparky's age
-console.log(sparkyAge);
+console.log(sparkyAge); //write the var sparkyAge to the console
 
 //Slice of Pie Part 1
-
+var pizza = 4; //number of pizzas
+var slices = 10; //number of slices per pizza
+var people = 5; //number of people at the party
+var slicesEaten = pizza * slices / people; //how many slices did they eat?
+console.log("Each person ate " + slicesEaten + "slices of pizza at the party.") //write "Each person ate 8 slices of pizza at the party." to the console
 
 //Slice of Pie Part 2
 
