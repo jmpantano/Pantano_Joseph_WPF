a27f9e3901383e1969a38082e39b7d0992bc3bf7
Joseph Pantano <joseph.pantano@live.com>
2014-08-13 13:54:40 -0700

    Initial commit with Dog Years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,18 @@
+//Joseph Pantano 13Aug2014 Expressions Worksheet
+
+//Dog Years
+var years = 1; //human years
+var dogYears = years * 7; //dog years
+var sparkyAge = "Sparky is " + years + " human years old, which is " + dogYears + " in dog years."; //Sparky's age
+console.log(sparkyAge);
+
+//Slice of Pie Part 1
+
+
+//Slice of Pie Part 2
+
+
+//Average Shopping Bill
+
+
+//Discounts
